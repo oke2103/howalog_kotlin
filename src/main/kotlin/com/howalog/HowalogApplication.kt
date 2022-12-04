01073dba017807b7c312baf6a1a3b70de77f00b4
howan <oke2103@gmail.com>
2022-12-04 20:29:32 +0900

    first commit in howalog_kotlin

---
+++ src/main/kotlin/com/howalog/HowalogApplication.kt
@@ -0,0 +1,11 @@
+package com.howalog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HowalogApplication
+
+fun main(args: Array<String>) {
+    runApplication<HowalogApplication>(*args)
+}
---
+++ src/test/kotlin/com/howalog/HowalogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.howalog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HowalogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
